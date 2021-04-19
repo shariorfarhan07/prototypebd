430bbd507e23e9f0f88502e81d074d822edff613
Md Sharior Hossain Farhan <57462157+shariorHossainfarhan@users.noreply.github.com>
2021-04-20 05:36:50 +0600

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,5 +1,5 @@
 anchor.addEventListener('click',(e)=>{
-    console.log(155);
+
     e.preventDefault();
    if(form.name.value!==''){  
     console.log(15);
@@ -9,12 +9,19 @@ anchor.addEventListener('click',(e)=>{
             City:form.City.value,
             Zip:form.Zip.value,
             Phone:form.Phone.value,
-            Company:form.Company.value
+            Company:form.Company.value,
+            Date: firebase.firestore.Timestamp.fromDate(new Date())
         });
         form.name.value='';
+        form.style.display="none";
+        h2.innerHTML="Thank You For Placing Order"
        // window.setTimeout(location.reload(),1000);
-       console.log(155);
-       setTimeout("location.reload(true);", 1000);
+    //   var order=db.collection("order").doc('total');
+   // order.update({
+    //      order:firebase.firestore.FieldValue.increament(1)
+   // });
+   
+      // setTimeout("location.reload(true);", 1000);
     }
    
 })
